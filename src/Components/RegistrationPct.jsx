ba1ac427566e34d5206784a8b05c054a5693851d
Ayshafebin <febinayisha0@gmail.com>
2023-12-18 13:27:34 +0530

    scnd

--- src/Components/RegistrationPct.jsx
+++ src/Components/RegistrationPct.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RegistrationPct = () => {
+
+    const [name,setName] = useState([]);
+    const [email,setEmail] = useState([]);
+    const [password,setPassword] = useState([]);
+
+    const handleform = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+    }
+
   return (
-    <div>RegistrationPct</div>
+    <>
+    <form onSubmit={handleform}>
+        <label htmlFor="username">UserName</label>
+        <br />
+        <input 
+        type="text" 
+        id='username'
+        name='username'
+        /> <br />
+        <label htmlFor="email">Email</label>
+        <br />
+        <input 
+        type="email"
+        id='email'
+        name='email'
+        /> <br />
+        <label htmlFor="password">Password</label> <br />
+        <input 
+        type="text" 
+        id='password'
+        name='password'
+        />
+        <br /> <br />
+        <button>Submit</button>
+    </form>
+    </>
   )
 }
 
